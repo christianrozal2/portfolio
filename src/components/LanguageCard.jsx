5e40a2feb58c7ae6bdc10245d0f075844118f45f
christianrozal2 <christianrozal2@gmail.com>
2024-03-03 14:35:07 +0800

    Edited languagecard

--- src/components/LanguageCard.jsx
+++ src/components/LanguageCard.jsx
@@ -5,23 +5,25 @@ const LanguageCard = ({card}) => {
     const { title, subtitle, description, image, link } = card
   return (
     <a href={link} target="_blank">
-        <div className='flex sm:w-[413px] w-[250px] bg-container p-5 rounded-2xl hover:bg-hover1 transition duration-300 ease-in-out cursor-pointer'>
-            <div className='flex flex-col gap-3'>
-                <h3 className='gradient2text sm:text-2xl text-xl font-semibold'>
-                    {title}
-                </h3>
-                <h4 className='italic sm:text-lg text-base'>
-                    {subtitle}
-                </h4>
-                <p className='text-text1 sm:text-base text-sm'>
-                    {description}
-                </p>
-                <div className='flex items-center'>
-                    <p className='sm:text-base text-sm'>
-                        Check documentation
+        <div className='flex sm:w-[413px] w-[250px] h-full bg-container p-5 rounded-2xl hover:bg-hover1 transition duration-300 ease-in-out cursor-pointer'>
+            <div className='flex flex-col gap-3 justify-between'>
+                <div className='flex flex-col gap-3'>
+                    <h3 className='gradient2text sm:text-2xl text-xl font-semibold'>
+                        {title}
+                    </h3>
+                    <h4 className='italic sm:text-lg text-base'>
+                        {subtitle}
+                    </h4>
+                    <p className='text-text1 sm:text-base text-sm'>
+                        {description}
                     </p>
-                    <img src={arrow} alt="arrow" className='size-6'/>
                 </div>
+                <div className='flex items-center'>
+                        <p className='sm:text-base text-sm'>
+                            Check documentation
+                        </p>
+                        <img src={arrow} alt="arrow" className='size-6'/>
+                    </div>
             </div>
             <div className='self-end w-[100%]'>
                 <img src={image} alt={title} />
