b124a2ca9acc1a5355776e6f44d3e63ee1a442b6
christianrozal2 <christianrozal2@gmail.com>
2024-03-06 08:45:52 +0800

    Updated toolcard heading tag

--- src/components/ToolCard.jsx
+++ src/components/ToolCard.jsx
@@ -7,14 +7,14 @@ const ToolCard = ({card}) => {
     <a href={link} target="_blank">
       <div className='flex flex-col sm:w-[280px] w-[250px] bg-container rounded-2xl hover:bg-hover1 transition duration-300 ease-in-out cursor-pointer text-center'>
           <div className='sm:w-[280px] w-[250px] text-center py-2 rounded-t-2xl bg-white gradient3'>
-                  <h3 className='text-text2 font-semibold sm:text-lg text-base'>{title1}</h3>
+                  <h2 className='text-text2 font-semibold sm:text-lg text-base'>{title1}</h2>
           </div>
           
           <div className='flex flex-col items-center p-8'>
               <div className='rounded-full bg-hover1 p-10'>
                   <img loading='lazy' src={image} alt={title1} className='sm:w-[80px] w-[60px] h-auto' />
               </div>
-              <h1 className='font-bold mt-5 uppercase textGradient sm:text-base text-sm'>{title2}</h1>
+              <h3 className='font-bold mt-5 uppercase textGradient sm:text-base text-sm'>{title2}</h3>
               <p className='text-gray-400 italic sm:text-base text-sm'>{developer}</p>
               <div className='bg-body py-2 w-full rounded-lg mt-3 flex justify-center items-center'>
                   <p className='sm:text-base text-sm'>Learn More</p>
